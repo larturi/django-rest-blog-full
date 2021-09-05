f59c22a4d0c860847a1bc29b59e05643128af464
lea.arturi <lea.arturi@gmail.com>
2021-09-05 19:33:38 -0300

    Modelo User listo

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -22,3 +22,9 @@ class UserSrializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'first_name', 'last_name']
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
--- user/api/views.py
+++ user/api/views.py
@@ -2,7 +2,12 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from user.api.serializers import UserRegisterSerializer, UserSrializer
+from user.models import User
+from user.api.serializers import (
+    UserRegisterSerializer,
+    UserSrializer,
+    UserUpdateSerializer
+)
 
 
 class RegisterView(APIView):
@@ -20,5 +25,14 @@ class UserView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        serializers = UserSrializer(request.user)
-        return Response(serializers.data)
+        serializer = UserSrializer(request.user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = User.objects.get(id=request.user.id)
+        serializer = UserUpdateSerializer(user, request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
