fe174782fc5e392f95f0e5c20e9f7289992e7d6c
lea.arturi <lea.arturi@gmail.com>
2021-09-06 14:15:42 -0300

    [Despliegue] - requirements.txt

--- blog/settings.py
+++ blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t*r1rpovcl0rdq*h&(y96o*g_tp5ne=%t6)r)&knp+-v)$%(^j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
