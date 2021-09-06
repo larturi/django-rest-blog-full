115a8439a67145376fa1cc64b43f3fc39a860795
lea.arturi <lea.arturi@gmail.com>
2021-09-06 11:37:23 -0300

    Filtros en category

--- category/api/views.py
+++ category/api/views.py
@@ -2,10 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 from category.api.serializers import CategorySerializer
 from category.models import Category
 from category.api.permissions import IsAdminOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CategoryApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()
+    queryset = Category.objects.filter(published=True)
     lookup_field = 'slug'
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['slug']
