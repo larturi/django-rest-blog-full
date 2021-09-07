7550a824fd8a8a237954752537232fad9ce1fb20
lea.arturi <lea.arturi@gmail.com>
2021-09-06 23:29:37 -0300

    *.js linguist-language=Python

--- post/api/serializers.py
+++ post/api/serializers.py
@@ -3,8 +3,26 @@ from post.models import Post
 from user.models import User
 from category.models import Category
 
+from user.api.serializers import UserSerializer
+from category.api.serializers import CategorySerializer
+
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['id', ]
+
+
+# class CategorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = ['id', ]
+
 
 class PostSerializer(serializers.ModelSerializer):
+    # user = UserSerializer()
+    # category = CategorySerializer()
+
     class Meta:
         model = Post
         depth = 2
@@ -17,5 +35,5 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at',
             'published',
             'category',
-            'user',
+            'user'
         ]
