0626d0e65eee8fdb23a55ae1a1e0de57f3d4b24d
lea.arturi <lea.arturi@gmail.com>
2021-09-09 09:48:48 -0300

    Fix nested model comment

--- comment/api/serializers.py
+++ comment/api/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
 from comment.models import Comment
 from user.api.serializers import UserSerializer
-from category.api.serializers import CategorySerializer
+from post.api.serializers import PostSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    # user = UserSerializer()
-    # category = CategorySerializer()
+
+    user_data = UserSerializer(source="user", read_only=True)
+    post_data = PostSerializer(source="post", read_only=True)
 
     class Meta:
         model = Comment
-        fields = ['content', 'user', 'post', 'created_at']
+        fields = [
+            'content',
+            'user',
+            'user_data',
+            'post',
+            'post_data',
+            'created_at'
+        ]
--- post/api/serializers.py
+++ post/api/serializers.py
@@ -1,11 +1,13 @@
-from category.api.serializers import CategorySerializer
 from rest_framework import serializers
 from post.models import Post
+from category.api.serializers import CategorySerializer
+from user.api.serializers import UserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
 
     category_data = CategorySerializer(source="category", read_only=True)
+    user_data = UserSerializer(source="user", read_only=True)
 
     class Meta:
         model = Post
@@ -17,6 +19,7 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at',
             'published',
             'user',
+            'user_data',
             'category',
             'category_data'
         ]
